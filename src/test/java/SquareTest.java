ad7b94db667082e7ec10eff2d8b5cd66202d0101
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2023-10-23 10:31:39 +0400

    добавления простой проверки

--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -6,6 +6,7 @@ public class SquareTest {
     @Test
     public void testArea() {
         Square s = new Square(5);
+        assert s.area() == 25;//более простая схема сравнения
         Assert.assertEquals(s.area(),25);
     }
 }
