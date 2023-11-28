c9c54e1a8e1a42dd8b976daf102893ad6474176f
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2023-11-28 11:40:12 +0400

    Добавление проверок списков групп

--- addressbook-web-tests/src/test/java/addressbook/Tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/addressbook/Tests/GroupDeletionTest.java
@@ -1,6 +1,11 @@
 package addressbook.Tests;
 
+import addressbook.model.GroupData;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class GroupDeletionTest extends TestBase {
 
@@ -15,5 +20,18 @@ public class GroupDeletionTest extends TestBase {
         }
         app.SelectGroup();
         //Добавить метод удаления группы
+
+        List<GroupData> before = app.getGroupHelper().getGroupList();//Получение массива с элементами
+        app.getGroupHelper().selectGrou(before.size() -1); //Вычерание еденицы из колличества элементов в массиве , запись результата в переменную
+        app.getGroupHelper().deleteSelectedGroups();//Удаление выбранной группы
+        app.getGroupHelper().returnToGroupPage();//Переход на страницу групп
+        List<GroupData> after = app.getGroupHelper().getGroupList();//Получение массива с элементами
+        Assert.assertEquals(after.size(), before.size() -1);//проверка что количество элементов полученных в массиве после удаления равна количеству элементов в массиве до удаления -1
+
+        before.remove(before.size()-1);//Удаление элемента из списка
+        Assert.assertEquals(before, after);//Проверка что элемент старого списка равен элементу нового списка
+
+
+
     }
 }
---
+++ addressbook-web-tests/src/test/java/addressbook/model/GroupData.java
@@ -0,0 +1,20 @@
+package addressbook.model;
+
+public class GroupData {
+    @Override //Позволяет выводить текст элементов при скравнение элементов в массиве а не их номер в ячейке памяти
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name +'\''+
+                '}';
+    }
+
+    @Override //Позволяет сравнивать объекты
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+
+}
