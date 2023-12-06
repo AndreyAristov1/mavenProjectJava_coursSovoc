ea72fc97b2ead37984cd29b05a758b5279f7db2d
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2023-12-06 10:56:52 +0400

    Добавление лямбда выражения

--- addressbook-web-tests/src/test/java/addressbook/Tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/Tests/GroupCreationTests.java
@@ -1,9 +1,12 @@
 package addressbook.Tests;
 
+import addressbook.model.GroupData;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.util.Comparator;
 
 public class GroupCreationTests extends TestBase {
 
@@ -20,6 +23,16 @@ public class GroupCreationTests extends TestBase {
         int after = app.getGroupHelper().getGroupCount();//Количество групп после добавления
         Assert.assertEquals(after, before +1);//Проверка что количество групп равно имеющемуся количеству +1
     }*/
+
+        //Поиск элемента с максимальным идентефекатором
+        /*int max =0;
+        for(GroupDatag g : after){
+            if(g.getId() > max){
+             max = g.getId();
+            }
+        }*/
+
+       // int max = after.stream().max((o1, o2) -> integer.compare(o1.getId(), o2.getId())).get().getId();
 
 /*
     //Получение количества выбранных элементов на странице
