abcefda2d5f461f573409fcf1fb096346b83d7c1
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2024-01-17 11:02:52 +0400

    Получение данных из БД

--- addressbook-web-tests/src/test/java/addressbook/Tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/addressbook/Tests/DbConnectionTest.java
@@ -1,28 +1,53 @@
 package addressbook.Tests;
 
+import addressbook.model.GroupData;
+import addressbook.model.Groups;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.*;
-import java.io.*;
+
+import static javax.swing.UIManager.getInt;
+
 
 public class DbConnectionTest {
 
+    //Инициализация драйвера
+    public class LoadDriver {
+        public static void main(String[] args) {
+            try {
+
+
+                Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            } catch (Exception ex) {
+
+            }
+        }
+    }
+
     @Test
 
+//Тест что БД подключена
     public void testDbConnection() {
         Connection conn = null;
 
         try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+            //Получение информации из БД
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list ");
 
+            //Получение информации из столбцов
+            Groups groups = new Groups();
+            while (rs.next()) {
+                groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                        .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+            }
+            //Завершение работы с БД
+            rs.close();
+            st.close();
+            conn.close();
 
-            conn =
-                    DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
-
-            // Do something with the Connection
-
+            System.out.println(groups);
 
         } catch (SQLException ex) {
             // handle any errors
